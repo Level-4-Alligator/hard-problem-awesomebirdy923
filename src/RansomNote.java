881815672c15429cc924abbd6c26fff2f50267d9
Cole Rudnick <league@WTS19.attlocal.net>
2017-05-25 18:26:21 -0700

    a
    a

--- src/RansomNote.java
+++ src/RansomNote.java
@@ -3,11 +3,31 @@ import java.util.*;
 public class RansomNote {
 	Map<String, Integer> magazineMap;
 	Map<String, Integer> noteMap;
+	String mag;
+	String not;
 
 	public RansomNote(String magazine, String note) {
+		mag = magazine;
+		not = note;
+		magazineMap = new HashMap();
+		noteMap = new HashMap();
+
 	}
 
 	public boolean solve() {
+		for (int i = 0; i < mag.split(" ").length; i++) {
+			magazineMap.put(mag.split(" ")[i], i);
+		}
+		for (int i = 0; i < not.split(" ").length; i++) {
+			noteMap.put(not.split(" ")[i], i);
+		}
+		// ---------------------------------------------------------
+		ArrayList<String> magazine = (ArrayList<String>) magazineMap.keySet();
+		ArrayList<String> note = (ArrayList<String>) noteMap.keySet();
+		if (magazine.get(5)==note.get(5)) {
+			return false;
+		}
+		return true;
 	}
 
 	public static void main(String[] args) {
